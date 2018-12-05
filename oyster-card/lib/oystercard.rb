8464921518100a8e70943d49e2e4a58dec470a2b
Tom Russell <TR115251@hotmail.co.uk>
2018-12-05 15:20:25 +0000

    step 8, in_journey, touch in and touch out

--- oyster-card/lib/oystercard.rb
+++ oyster-card/lib/oystercard.rb
@@ -5,14 +5,27 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @cardstate = false
   end
 
   def top_up(value)
-    balance + value > MAX_BALANCE ? raise("top up exceeds maximum balance (#{Oystercard::MAX_BALANCE})") : @balance += value
+    balance + value > MAX_BALANCE ? raise("top up exceeds maximum balance(#{Oystercard::MAX_BALANCE}) by #{(balance + value - Oystercard::MAX_BALANCE)}") : @balance += value
   end
 
   def deduct(value)
-    @balance -= value 
+    @balance -= value
+  end
+
+  def in_journey?
+    @cardstate
+  end
+
+  def touch_in
+    @cardstate = true
+  end
+
+  def touch_out
+    @cardstate = false
   end
 
 end
--- oyster-card/spec/oystercard_spec.rb
+++ oyster-card/spec/oystercard_spec.rb
@@ -3,9 +3,13 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) { Oystercard.new }
-
-  it 'new cards have default balance 0' do
-    expect(card.balance).to eq(0)
+  context "initally" do
+    it 'has default balance 0' do
+      expect(card.balance).to eq(0)
+    end
+    it 'is not in journey' do
+      expect(card).not_to be_in_journey
+    end
   end
 
   describe '#top_up' do
@@ -16,9 +20,10 @@ describe Oystercard do
     it 'raises error when topping-up balance past maximum' do
       maximum_amount = (Oystercard::MAX_BALANCE) # shows calling constants outside of class (will eq 90)
       card.top_up(maximum_amount)
-      expect { card.top_up(1) }.to raise_error "top up exceeds maximum balance (#{maximum_amount})"
+      value = rand(1..10)
+      balance_by = (card.balance + value - (Oystercard::MAX_BALANCE))
+      expect { card.top_up(value) }.to raise_error "top up exceeds maximum balance(#{Oystercard::MAX_BALANCE}) by #{balance_by}"
     end
-
   end
 
   describe '#deduct' do
@@ -26,6 +31,19 @@ describe Oystercard do
       card.top_up(Oystercard::MAX_BALANCE)
       value = 50
       expect { card.deduct(value) }.to change { card.balance }.by(-value)
+    end
+  end
+
+  describe '#injourney' do
+    it ' true after touch_in' do
+      expect { card.touch_in }.to change { card.in_journey? }.from(false).to(true)
+    end
+  end
+
+  describe '#injourney' do
+    it 'false after touch_in then touch_out' do
+      card.touch_in
+      expect { card.touch_out }.to change { card.in_journey? }.from(true).to(false)
     end
   end
 
