fc679dd2dda011e0c039559fa2ccc8720bca2cf4
Tom Russell <TR115251@hotmail.co.uk>
2018-12-05 18:40:42 +0000

    green dots

--- oyster-card/lib/oystercard.rb
+++ oyster-card/lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
-  attr_accessor :balance
+  attr_accessor :balance, :in_station
   LIMIT = 90
   MINIMUM = 1
 
   def initialize
     @balance = 0
     @cardstate = false
+    @in_station = nil
+    # @out_station = nil
   end
 
   def top_up(value)
@@ -18,8 +20,9 @@ class Oystercard
     @cardstate
   end
 
-  def touch_in
-   balance < MINIMUM ? raise("Balance less than (£#{MINIMUM}) Please top up") : @cardstate = true
+  def touch_in(station)
+    balance < MINIMUM ? raise("Balance less than (£#{MINIMUM}) Please top up") : @cardstate = true
+    @in_station = station 
   end
 
   def touch_out
--- oyster-card/spec/oystercard_spec.rb
+++ oyster-card/spec/oystercard_spec.rb
@@ -29,9 +29,9 @@ describe Oystercard do
   # private method cannot be tested
   # describe '#deduct' do
   #   it 'deducts value from balance' do
-  #     card.top_up(Oystercard::LIMIT)
-  #     value = rand(1..90)
-  #     expect { card.deduct(value) }.to change { card.balance }.by(-value)
+  #     # card.top_up(Oystercard::LIMIT)
+  #     # value = rand(1..90)
+  #     # expect { card.deduct(value) }.to change { card.balance }.by(-value)
   #   end
   # end
 
@@ -39,25 +39,35 @@ describe Oystercard do
     card = Oystercard.new
     card.top_up(Oystercard::LIMIT)
     it ' true after touch_in' do
-      expect { card.touch_in }.to change { card.in_journey? }.from(false).to(true)
+      station = double(:Station, station_name: "big_london_station")
+      expect { card.touch_in(station) }.to change { card.in_journey? }.from(false).to(true)
     end
 
     it 'false after touch_in then touch_out' do
-      card.touch_in
+      station = double(:Station, station_name: "big_london_station")
+      card.touch_in(station)
       expect { card.touch_out }.to change { card.in_journey? }.from(true).to(false)
     end
   end
 
   describe '#touch_in' do
+    card = Oystercard.new
+    let(:station) { double(:Station, station_name: "big_london_station") }
     it " won't allow to touch_in if balance lower than minimum" do
-      expect { card.touch_in }.to raise_error "Balance less than (£#{Oystercard::MINIMUM}) Please top up"
+      expect { card.touch_in(station) }.to raise_error "Balance less than (£#{Oystercard::MINIMUM}) Please top up"
+    end
+
+    it " Remembers touch_in station" do
+      card.top_up(Oystercard::LIMIT)
+      expect { card.touch_in(station.station_name) }.to change { card.in_station }.from(nil).to("big_london_station")
     end
   end
 
   describe '#touch_out' do
     it " will change the balance by the minimum fare" do
+      station = double(:Station, station_name: "big_london_station") 
       card.top_up(Oystercard::LIMIT)
-      card.touch_in
+      card.touch_in(station)
       expect { card.touch_out }.to change { card.balance }.by(-Oystercard::MINIMUM)
     end
   end
